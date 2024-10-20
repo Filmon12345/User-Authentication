d72b4767316c8bf2b409a148473c792a1dd630fd
Filmonzinabu <zimilifila3676@gmail.com>
2024-10-20 17:55:25 +0300

    module Configuration

---
+++ app.js
@@ -0,0 +1,57 @@
+var express = require('express');
+var path = require('path');
+var expressValidator = require('express-validator');
+var session = require('express-session');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var bodyParser = require('body-parser');
+var flash = require('connect-flash');
+
+
+var routes = require('./routes/index');
+var users = require('./routes/users');
+
+var app = express();
+
+// view engine
+app.set('views' , path.join(__dirname,'views'));
+app.set('view engine' , 'ejs');
+
+//set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+  
+// bodyparser
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false}));
+
+
+app.use(session({
+    secret: 'secret',
+    saveUnitialized: true,
+    resave: true
+
+}));
+
+// passport Middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+app.use(expressValidator({
+    errorFormatter: function(param, msg, value){
+        var namespace = param.split('.')
+        , root = namespace.shift()
+        , formParam = root;
+
+        while(namespace.length){
+            formParam += '[' + namespace.shift() +']';
+        }
+        return{
+        param : formParam,
+        msg : msg,
+        value : value
+
+    };
+}
+}));
